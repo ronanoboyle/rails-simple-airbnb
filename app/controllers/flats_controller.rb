3e7606899005a75d75334ad82ae6c51e97928cd9
Ronan OBoyle <ronanoboyle.rob@gmail.com>
2023-02-12 16:00:28 +0000

    Working on show page

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,25 @@
 class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    flat = Flat.new(flat_params)
+    flat.save
+    redirect_to flats_path(flat)
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
